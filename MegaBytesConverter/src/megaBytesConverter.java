0f0edcff0ef95bb732f07f9fc949b0aa30cba3a2
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2019-10-15 19:24:50 -0700

    MB converter

---
+++ MegaBytesConverter/src/megaBytesConverter.java
@@ -0,0 +1,11 @@
+public class megaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + "KB = " + kiloBytes / 1024 + "MB and " + (kiloBytes - kiloBytes / 1024 * 1024));
+        }
+    }
+}
